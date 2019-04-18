c9f1503ded7912e1afba42b530a628459d53d545
Elijsha Baetiong <baet6458@kettering.edu>
2019-04-18 11:58:02 -0400

    Added logs to see the life cycles of the App

--- app/src/main/java/com/ebaetiong/twoactivities/SecondActivity.java
+++ app/src/main/java/com/ebaetiong/twoactivities/SecondActivity.java
@@ -3,6 +3,7 @@ package com.ebaetiong.twoactivities;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -12,6 +13,44 @@ public class SecondActivity extends AppCompatActivity {
     public static final String EXTRA_REPLY ="com.ebaetiong.twoactivities.extra.REPLY";
 
     private EditText mReply;
+
+    private static final String LOG_TAG = SecondActivity.class.getSimpleName();
+
+    @Override
+    public void onStart(){
+        super.onStart();
+        Log.d(LOG_TAG, "onStart");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(LOG_TAG, "onPause");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(LOG_TAG,"onRestart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(LOG_TAG,"onResume");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(LOG_TAG,"onStop");
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        Log.d(LOG_TAG,"onDestroy");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +68,7 @@ public class SecondActivity extends AppCompatActivity {
         Intent replyIntent = new Intent();
         replyIntent.putExtra(EXTRA_REPLY, reply);
         setResult(RESULT_OK,replyIntent);
+        Log.d(LOG_TAG,"End SecondActivity");
         finish();
     }
 }
